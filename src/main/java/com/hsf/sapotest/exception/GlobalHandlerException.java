89d94bdb5725604eeee6b9eed41998080bd294d6
HungNM <lalala123hung@gmail.com>
2026-03-14 15:08:27 +0700

    a

---
+++ src/main/java/com/hsf/sapotest/exception/GlobalHandlerException.java
@@ -0,0 +1,25 @@
+package com.hsf.sapotest.exception;
+
+import com.hsf.sapotest.dto.response.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler(value = { InvalidDataException.class, Exception.class })
+    public ErrorResponse handleException(Exception e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setError(HttpStatus.CONFLICT.getReasonPhrase());
+        errorResponse.setStatus(HttpStatus.CONFLICT.value());
+        errorResponse.setPath(request.getDescription(false).replace("uri=",""));
+        errorResponse.setTimestamp(new Date());
+
+        return errorResponse;
+    }
+}
